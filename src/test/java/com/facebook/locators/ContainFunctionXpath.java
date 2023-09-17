250904d4030ae879bae7985a3772cd6bb0a52c6b
rezamazid <rezamazid7869@gmail.com>
2023-09-17 11:25:02 -0400

    git branching

---
+++ src/test/java/com/facebook/locators/ContainFunctionXpath.java
@@ -0,0 +1,12 @@
+package com.facebook.locators;
+
+import org.testng.annotations.Test;
+
+public class ContainFunctionXpath {
+	
+	@Test
+	public void containFunction() {
+		System.out.println("git branching");
+	}
+
+}
